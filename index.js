2adefaa280676f46fd4006ba90667204c6db93be
Олег Дутченко <dutchenko.o.dev@gmail.com>
2019-06-18 15:30:57 +0300

    3.1.0 (#3)
    * upd .gitignore file
    
    * upd dependencies
    
    * implement interface
    
    * jsdoc - TippySettings
    
    * 3.1.0

--- index.js
+++ index.js
@@ -81,6 +81,11 @@ export class AbstractTippy extends WebPluginInterface {
 	}
 
 	/** @protected */
+	_initialize () {
+		this.tooltip = tippy(this.element, this.settings);
+	}
+
+	/** @protected */
 	_afterInitialize () {
 		super._afterInitialize();
 	}
@@ -89,13 +94,61 @@ export class AbstractTippy extends WebPluginInterface {
 	initialize () {
 		this._setup();
 		this._beforeInitialize();
-		this.tooltip = tippy(this.element, this.settings);
+		this._initialize();
 		this._afterInitialize();
 	}
 }
 
 /**
  * @typedef {Options} TippySettings
+ * @property {boolean} [a11y] true
+ * @property {boolean} [allowHTML] true
+ * @property {boolean} [animateFill] true
+ * @property {string} [animation] shift-away
+ * @property {string} [appendTo] function appendTo() {return document.body}
+ * @property {string} [aria] describedby
+ * @property {boolean} [arrow] false
+ * @property {string} [arrowType] sharp
+ * @property {string} [boundary] scrollParent
+ * @property {string} [content]
+ * @property {number} [delay] 0
+ * @property {number} [distance] 10
+ * @property {number[]} [duration] [325, 275]
+ * @property {boolean} [flip] true
+ * @property {string} [flipBehavior] flip
+ * @property {boolean} [flipOnUpdate] false
+ * @property {boolean} [followCursor] false
+ * @property {boolean} [hideOnClick] true
+ * @property {boolean} [ignoreAttributes] false
+ * @property {boolean} [inertia] false
+ * @property {boolean} [interactive] false
+ * @property {number} [interactiveBorder] 2
+ * @property {number} [interactiveDebounce] 0
+ * @property {boolean} [lazy] true
+ * @property {number} [maxWidth] 350
+ * @property {boolean} [multiple] false
+ * @property {number} [offset] 0
+ * @property {function} [onHidden]
+ * @property {function} [onHide]
+ * @property {function} [onMount]
+ * @property {function} [onShow]
+ * @property {function} [onShown]
+ * @property {function} [onTrigger]
+ * @property {string} [placement] top
+ * @property {object} [popperOptions] {}
+ * @property {string} [role] tooltip
+ * @property {boolean} [showOnInit] false
+ * @property {string} [size] regular
+ * @property {boolean} [sticky] false
+ * @property {string} [target]
+ * @property {string} [theme] dark
+ * @property {boolean} [touch] true
+ * @property {boolean} [touchHold] false
+ * @property {string} [trigger] mouseenter focus
+ * @property {null} [triggerTarget] null
+ * @property {number} [updateDuration] 0
+ * @property {null} [wait] null
+ * @property {number} [zIndex] 9999
  */
 
 /**
